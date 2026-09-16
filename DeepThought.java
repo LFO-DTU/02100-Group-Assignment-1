e8c58be76024e8e25cfe8a5a1bac46ab4a21b299
LFO-DTU <s266014@dtu.dk>
2026-09-16 12:49:46 +0200

    Fixed Opg 2

--- DeepThought.java
+++ DeepThought.java
@@ -1,22 +1,12 @@
-/*public class UltimateQuestion {
-		
-	public static void main(String[] args) {
-		
-		// Deep Thought parameters
-		double x = 7.2;
-		System.out.println(1 + 3 + x + x + x + x + x + 5 /4 * 2);
-	}
 
-
-}*/
 
 public class UltimateQuestion {
 		
 	public static void main(String[] args) {
 		
 		// Deep Thought parameters
-		double x = 7.2;
-		System.out.println((int)(1 + 3 + x + x + x + x + x + 5 /4 * 2));
+		String x = "";
+		System.out.println((1 + 3 + x + x + x + x + x + 5 /4 * 2));
 	}
 
 
@@ -24,4 +14,4 @@ public class UltimateQuestion {
 
 
 
-/**/
+
